06c66bce04cfcc65c7d72e494ea8f688557445fb
logan1x <sharmakhushal78@gmail.com>
2021-08-20 12:35:30 +0530

    mobile responsive menu

--- app.js
+++ app.js
@@ -8,29 +8,43 @@ const numOfNotes = document.querySelectorAll(".num-of-notes");
 const notes = [1000, 500, 100, 20, 10, 5, 2, 1];
 
 formSubmit.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const bill = Number(billGiven.value);
-	const cash = Number(cashGiven.value);
+  e.preventDefault();
+  const bill = Number(billGiven.value);
+  const cash = Number(cashGiven.value);
 
-	console.log(bill < cash);
+  console.log(bill < cash);
 
-	if (bill < 0) {
-		alert("Please enter a valid bill amount");
-		return;
-	} else if (cash < bill) {
-		alert("Please enter a valid cash amount");
-		return;
-	}
+  if (bill < 0) {
+    alert("Please enter a valid bill amount");
+    return;
+  } else if (cash < bill) {
+    alert("Please enter a valid cash amount");
+    return;
+  }
 
-	const change = cash - bill;
+  const change = cash - bill;
 
-	calcBillamount(change);
+  calcBillamount(change);
 });
 
 function calcBillamount(change) {
-	for (var i = 0; i < notes.length; i++) {
-		const numberOfNotes = Math.trunc(change / notes[i]);
-		change %= notes[i];
-		numOfNotes[i].innerText = numberOfNotes;
-	}
+  for (var i = 0; i < notes.length; i++) {
+    const numberOfNotes = Math.trunc(change / notes[i]);
+    change %= notes[i];
+    numOfNotes[i].innerText = numberOfNotes;
+  }
 }
+
+// for mobile nav
+
+// get variables
+
+const btn = document.querySelector("button.mobile-nav-btn");
+const menu = document.querySelector(".mobile-nav");
+
+// event toggle
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  console.log("clicked");
+});
